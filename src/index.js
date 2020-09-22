188d8e09d7dbbbe33f35cf239f9a81fda71967fd
kowshik007 <kowshikkondapaneni@gmail.com>
2020-09-22 08:42:15 +0530

    09/22/2020 [Kowshik] adding state

--- src/index.js
+++ src/index.js
@@ -1,18 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-class App extends React.Component{
-    render(){
-        window.navigator.geolocation.getCurrentPosition(
-            position=>console.log(position),
-            err=>console.log(err)
-        );
-        return (
-            <div>
-                Hi There
-            </div>
-        );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      errorMessage: "",
+    };
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+        });
+      },
+      (err) => {
+        this.setState({
+          errorMessage: err.message,
+        });
+      }
+    );
+  }
+  render() {
+    if(!this.state.lat && this.state.errorMessage){
+         return <div>Error: {this.state.errorMessage}</div>;
     }
+
+    if(this.state.lat && !this.state.errorMessage){
+        return <div>Latitude: {this.state.lat}</div>;
+    }
+    return (
+      <div>
+        Loading..
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App/>,document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
